c9868d90a0e459b6bef1552fe96999e9bf96004b
SamShebin <94579049+SamShebin@users.noreply.github.com>
2023-05-16 20:59:05 -0700

    Update template.s

--- template.s
+++ template.s
@@ -37,26 +37,31 @@ SWAP:
     	//   X1: The address of (pointer to) the second value to be swapped.
 	
 	// INSERT YOUR CODE HERE
-	SUBI SP,SP #40
-	STUR FP,[SP,#0]
-	STUR LR,[SP,#8]
-	STUR X0,[SP,#16]
-	STUR X1,[SP,#24]
-	ADDI FP,SP,#32
+SUBI SP,SP #40
+STUR FP,[SP,#0]
+STUR LR,[SP,#8]
+STUR X0,[SP,#16]
+STUR X1,[SP,#24]
+ADDI FP,SP,#32
 
-	LDUR X9,[X0,#0]
-	LDUR X10,[X1,#0]
-	STUR X10,[X0,#0]
-	STUR X9,[X1,#0]
+LDUR X9,[X0,#0]
+LDUR X10,[X1,#0]
+STUR X10,[X0,#0]
+STUR X9,[X1,#0]
+
+LDUR X19 [X0,#0] // PRINTING FOR TESTING
+LDUR X20 [X1,#0]
+putint X19
+putint X20       // PRINTING FOR TESTING
 
 SWAPEND:
-	LDUR FP,[SP,#0]
-	LDUR LR,[SP,#8]
-	LDUR X0,[SP,#16]
-	LDUR X1,[SP,#24]
-	ADDI SP,SP,#40
+LDUR FP,[SP,#0]
+LDUR LR,[SP,#8]
+LDUR X0,[SP,#16]
+LDUR X1,[SP,#24]
+ADDI SP,SP,#40
 	
-	BR LR
+BR LR
 
 ////////////////////////////
 //                    	//
